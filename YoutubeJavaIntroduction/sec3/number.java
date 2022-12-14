0647ebe1521187db795d002df7ea03e01f7ca895
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-14 09:05:01 +0900

    add number

---
+++ YoutubeJavaIntroduction/sec3/number.java
@@ -0,0 +1,7 @@
+public class number{
+    public static void main(String[] args){
+        System.out.println(10);
+        System.out.println(10 + 5);
+        System.out.println("10 + 5");
+    }
+}
