1a73c30b6c99c49116fd49ec535cd0df3bc3cfb8
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-14 18:55:56 +0900

    add statlf2

---
+++ documentIntroduce/sec3/Statlf2.java
@@ -0,0 +1,10 @@
+public class Statlf2 {
+    public static void main(String[] args) {
+        int val = 11;
+        if (val == 10) {
+            System.out.println("変数valは10です。") ;
+        } else {
+            System.out.println("変数valは10ではありません。");
+        }
+    }
+}
