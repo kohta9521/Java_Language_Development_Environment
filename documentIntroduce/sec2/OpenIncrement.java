7f81f240fc5030303d0fcf9017e0afe5f82fef93
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-14 14:58:30 +0900

    add OpneIncrement

---
+++ documentIntroduce/sec2/OpenIncrement.java
@@ -0,0 +1,10 @@
+public class OpenIncrement {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = x ++;
+        System.out.println("yは" + y);
+        int a = 1;
+        int b = ++ a;
+        System.out.println("bは" + b);
+    }
+}
