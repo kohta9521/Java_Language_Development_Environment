336a945f5979c5db014c552be2a6258205ffbaaa
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-14 14:54:05 +0900

    add type cast

---
+++ documentIntroduce/sec2/Cast.java
@@ -0,0 +1,7 @@
+public class Cast{
+    public static void main(String[] args) {
+        long l = 50;
+        int i = (int)l;
+        System.out.println(l + 1);
+    }
+}
---
+++ documentIntroduce/sec2/VarBasic.java
@@ -0,0 +1,6 @@
+public class VarBasic {
+    public static void main(String[] args) {
+        var num = 3.14;
+        System.out.println(num);
+    }
+}
