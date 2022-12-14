f29823c66edb2c4beaaf16b85b75245898ea8794
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-14 14:35:49 +0900

    add equal.java

---
+++ YoutubeJavaIntroduction/sec5/equal.java
@@ -0,0 +1,9 @@
+public class equal{
+    public static void main(String[] args){
+        int num = 10;
+        if(num == 10)
+            System.out.println("10と同じです。");
+        if(num != 10)
+            System.out.println("10と同じではないです。");
+    }
+}
