7364f803452ae47c7a15ba30aef8585eef6a84c5
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-16 16:27:13 +0900

    include hairetu

---
+++ superIntro/sec7/Lesson7_2.java
@@ -0,0 +1,9 @@
+class Lesson7_2 {
+    public static void main(String[] args) {
+        String[] arr = {"sato", "suzuki", "takahashi"};
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+    }
+}
