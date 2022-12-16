aa1c4df956ef4cb2f604927b92c2e49cba20f69b
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-16 16:39:35 +0900

    add || &&

---
+++ superIntro/sec8/Lesson8_4.java
@@ -0,0 +1,9 @@
+class Lesson8_4 {
+    public static void main(String[] args) {
+        int x = 8;
+        int y = 3;
+
+        System.out.println(x == 3 || y == 3);
+        System.out.println(x == 1 || y == 1);
+    }
+}
