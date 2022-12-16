6a0edac7e0ec4464d4253ff2cb3051da5b00d719
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-16 13:27:36 +0900

    add new dir sec6

---
+++ superIntro/sec6/Lesson7_1.java
@@ -0,0 +1,13 @@
+class Lesson7_1 {
+    public static void main(String[] args) {
+        byte val01 = 1;
+        short val02 = 12345;
+        int val03 = 1234567890;
+        long val04 = 123456789000L;
+
+        System.out.println(val01);
+        System.out.println(val02);
+        System.out.println(val03);
+        System.out.println(val04);
+    }
+}
