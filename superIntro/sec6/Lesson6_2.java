30b6a00733d3aa47abecb7efca726a34a753e7b1
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-16 14:53:33 +0900

    add char or String

---
+++ superIntro/sec6/Lesson6_2.java
@@ -0,0 +1,9 @@
+class Lesson6_2 {
+    public static void main(String[] args) {
+            char val01 = 'c';
+    String val02 = "Helo";
+
+    System.out.println(val01);
+    System.out.println(val02);
+    }
+}
