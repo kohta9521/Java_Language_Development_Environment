fc0893b4e3efe89887270ad41e07215f93f6181c
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-20 16:26:49 +0900

    add mew section and first file

--- introduction/sec3/Date.java
+++ introduction/sec3/Date.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 
 class Date {
     public static void main(String[] args) {
-        LocalDate Id = LocalDate.now();
+        LocalDate Id = LocalDate.now();  //  単純名
         System.out.println(Id);
     }
 }
