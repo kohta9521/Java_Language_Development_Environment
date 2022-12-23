928a872e3444951b85e75dd3bfb815ebf9e2157e
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-23 15:29:54 +0900

    add while

---
+++ introduction/sec7/Sample4.java
@@ -0,0 +1,10 @@
+class Sample4 {
+    public static void main(String[] args) {
+        int num = 1;
+
+        while (num < 100) {
+            System.out.println("num = " + num);
+            num *= 2;
+        }
+    }
+}
