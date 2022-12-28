58ddfc75e1bb22e7aee3fd2d8965fd39f610a00c
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-28 10:02:46 +0900

    add 整数リテラル　#75

---
+++ site/5.リテラル/3.エスケープシーケンス/Sample1.java
@@ -0,0 +1,8 @@
+class Sample1 {
+    public static void main(String[]args) {
+        System.out.println('\'');
+        System.out.println("私の名前は\"鈴木\"です");
+        System.out.println("この魚の値段は\\250円です");
+
+    }
+}
