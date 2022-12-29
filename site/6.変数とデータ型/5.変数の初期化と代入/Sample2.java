cfa05252a9fa78422ca308ac81b1664569f0c168
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-29 17:05:50 +0900

    add 変数の初期化と代入を同時に行う #75

---
+++ site/6.変数とデータ型/5.変数の初期化と代入/Sample2.java
@@ -0,0 +1,9 @@
+class Sample2 {
+    public static void main(String[] args) {
+        int old = 24;
+        String name = "Taro Yamada";
+
+        System.out.println(old);
+        System.out.println(name);
+    }
+}
