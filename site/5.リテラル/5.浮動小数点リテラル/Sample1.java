07d9282c2ae85fd6766fef84c2663425038d4631
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-29 16:41:02 +0900

    add 浮動小数点リテラル #754

---
+++ site/5.リテラル/5.浮動小数点リテラル/Sample1.java
@@ -0,0 +1,7 @@
+class Sample1 {
+    public static void main(String[] args) {
+        System.out.println(18.225);
+        System.out.println(5.23e3);
+        System.out.println(1.816e10);
+    }
+}
