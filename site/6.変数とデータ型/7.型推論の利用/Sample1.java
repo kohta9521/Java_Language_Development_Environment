c99ed95b935e955c8cc12e9cd54d5a902dbb93b3
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-29 17:14:54 +0900

    add var 型推論

---
+++ site/6.変数とデータ型/7.型推論の利用/Sample1.java
@@ -0,0 +1,9 @@
+class Sample1 {
+    public static void main(String[] args) {
+        var old = 24;
+        var name = "Yamada";
+
+        System.out.println("年齢 : " + old);
+        System.out.println("氏名 : " + name);
+    }
+}
