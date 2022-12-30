ae529f09f8d17a666c651463a2ea02a3dde60ebd
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-30 14:32:22 +0900

    演算子の優先順位

---
+++ site/7.演算子/3.演算子の優先順位/Sample1.java
@@ -0,0 +1,9 @@
+class Sample1 {
+    public static void main(String[] args) {
+        int n1 = 10 * 5 + 4;
+        int n2 = 4 + 10 * 5;
+
+        System.out.println("10 * 5 + 4 = " + n1);
+        System.out.println("4 + 10 * 5 = " + n2);
+    }
+}
