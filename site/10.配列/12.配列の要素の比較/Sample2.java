9677a4657b6f570da4104aef37741403fb30eea8
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-31 18:58:50 +0900

    多次元配列の一致 deepEqual

---
+++ site/10.配列/12.配列の要素の比較/Sample2.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Sample2 {
+    public static void main(String[] args) {
+        int[][] a = {{10, 8}, {9, 14}};
+        int[][] b = a;
+        int[][] c = {{10, 8}, {9, 14}};
+
+        System.out.println(Arrays.equals(a, b));
+        System.out.println(Arrays.equals(b, c));
+        System.out.println(Arrays.equals(a, c));
+        System.out.println(Arrays.deepEquals(a, b));
+        System.out.println(Arrays.deepEquals(a, c));
+        System.out.println(Arrays.deepEquals(b, c));
+    }
+}
