7074efddaceaefdf7e45391f5213845e5c9f6b7b
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-31 18:54:44 +0900

    arrayequals method

---
+++ site/10.配列/12.配列の要素の比較/Sample1.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Sample1 {
+    public static void main(String[] args) {
+        int[] a = {85, 78, 92};
+        int[] b = a;
+        int[] c = {85, 78, 92};
+
+        System.out.println(a == b);
+        System.out.println(a == c);
+        System.out.println(b == c);
+        System.out.println(Arrays.equals(a, b));
+        System.out.println(Arrays.equals(b, c));
+        System.out.println(Arrays.equals(b, c));
+    }
+}
