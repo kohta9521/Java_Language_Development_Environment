37caa9600db3ebfd1c416bebc32c26080a61fc73
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-31 17:27:51 +0900

    条件分岐　終了

---
+++ site/9.条件分岐/2.switch/Sample1.java
@@ -0,0 +1,21 @@
+class Sample1 {
+    public static void main(String[] args) {
+        int[] classcode = {1, 2, 3};
+        String[] name = {"鈴木", "本田", "遠藤"};
+
+        for (int i = 0; i < 3; i ++ ) {
+            System.out.print(name[i] + "さんは");
+
+            switch (classcode[i]) {
+                case 1:
+                    System.out.println("Aクラスです");
+                    break;
+                case 2:
+                    System.out.println("Bクラスです");
+                    break;
+                case 3:
+                    System.out.println("Cクラスです");
+            }
+        }
+    }
+}
