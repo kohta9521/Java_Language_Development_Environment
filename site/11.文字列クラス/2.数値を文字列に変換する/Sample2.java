f53b6f19291b051b15dd00f17d9653e93f5ef0a3
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-31 21:22:23 +0900

    String型への変換

---
+++ site/11.文字列クラス/2.数値を文字列に変換する/Sample2.java
@@ -0,0 +1,12 @@
+class Sample2 {
+    public static void main(String[] args) {
+        int i = 10;
+        short s = 21;
+
+        String si = String.valueOf(i);
+        String ss = String.valueOf(s);
+
+        System.out.println(si);
+        System.out.println(ss);
+    }
+}
