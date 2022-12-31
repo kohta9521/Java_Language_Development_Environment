632a9cda9c1ff2b2f4fe43de6f55671640006e12
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-31 17:51:32 +0900

    length

---
+++ site/10.配列/4.配列の長さ/Sample1.java
@@ -0,0 +1,13 @@
+class Sample1 {
+    public static void main(String[] args) {
+        double[] num = new double[3];
+
+        num[0] = 5.24;
+        num[1] = 8.0;
+        num[2] = 12.375;
+
+        for (int i = 0; i < num.length; i ++) {
+            System.out.println("num[" + i + "] = " + num[i]); 
+        }
+    }
+}
