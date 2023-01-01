413c159dd69758c735b37e823c49e3c118091942
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-01 11:42:09 +0900

    add new data

---
+++ site/12.メソッド/7.戻り値(配列)/Sample1.java
@@ -0,0 +1,9 @@
+class Sample1 {
+    public static void main(String[] args) {
+        int data[];
+
+        data = init();
+        System.out.println(data[0]);
+        System.out.println(data[1]);
+    }
+}
