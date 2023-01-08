5021e253ea85d1b3da45ee92055834c52f4f11c0
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-08 18:58:25 +0900

    インクリメント　デクリメントの前置　後置の違い

---
+++ bronze/2.変数・データ型・演算子/Sample7.java
@@ -0,0 +1,9 @@
+class Sample7 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a ++;
+        int c = ++a;
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
