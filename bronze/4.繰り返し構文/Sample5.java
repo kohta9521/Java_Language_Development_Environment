79634d4a1ad067c096f15690974f7a07516fecb7
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-08 21:40:43 +0900

    while

---
+++ bronze/4.繰り返し構文/Sample5.java
@@ -0,0 +1,9 @@
+class Sample5 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 2) {
+            System.out.println("i =  " + i);
+            i ++;
+        }
+    }
+}
