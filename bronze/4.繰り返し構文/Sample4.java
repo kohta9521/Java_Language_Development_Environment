4e4c655eab4d281a43c8a02b1bbbc56889188797
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-08 21:38:40 +0900

    for end

---
+++ bronze/4.繰り返し構文/Sample4.java
@@ -0,0 +1,9 @@
+class Sample4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i ++) {
+            for (int j = 0; j < 3; j ++) {
+                System.out.println("*");
+            }
+        }
+    }
+}
