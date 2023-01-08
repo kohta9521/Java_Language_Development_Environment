eab3d59170bc9c79adfc1c8f0bf292903eda741e
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-08 18:52:33 +0900

    算術演算子で文字列の連結

---
+++ bronze/2.変数・データ型・演算子/Sample6.java
@@ -0,0 +1,8 @@
+class Sample6 {
+    public static void main(String[] args) {
+        String a = "Hello, ";
+        String b = "Java";
+        String result = a + b + "!";
+        System.out.println(result);
+    }
+}
