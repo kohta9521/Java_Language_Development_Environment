fcfa1dfccc7e4967c5e35228ac118dfe0c16e0c2
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-09 16:48:44 +0900

    意図的なコンパイルエラー

---
+++ bronze/6.メソッド/Sample5.java
@@ -0,0 +1,9 @@
+class Sample5 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+    }
+    public static void test() {
+        int a = 20;
+    }
+}
---
+++ bronze/6.メソッド/Sample6.java
@@ -0,0 +1,9 @@
+class Sample6 {
+    public static void main(String[] args) {
+        int a = 10;
+        test();
+    }
+    public static void test(int  num) {
+        System.out.println(num);
+    }
+}
