87defc38da5fe1ea12b5d33a8c45ffc92f8ac782
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-09 16:21:43 +0900

    配列を作成し中身を取り出す

--- bronze/5.配列/Sample1.java
+++ bronze/5.配列/Sample1.java
@@ -1,5 +1,13 @@
 class Sample1 {
     public static void main(String[] args) {
-        
+        int[] array = new int[3];
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+
+        for (int i = 0; i < 3; i ++) {
+            int val = array[i];
+            System.out.println(val);
+        }
     }
 }
