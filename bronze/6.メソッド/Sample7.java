9af17db1b56d7e8fa7d6824126690bf10c9be0ec
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-09 16:52:50 +0900

    複数の引数お受け渡し

---
+++ bronze/6.メソッド/Sample7.java
@@ -0,0 +1,10 @@
+class Sample7 {
+    public static void main(String[] args) {
+        int a = 10;
+        noParameterMethod("10");
+    }
+
+    public static void noParameterMethod(int num) {
+        System.out.println("no");
+    }
+}
