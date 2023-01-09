0dd31c6ab9050d464b5ffe560fecdf06f205b15e
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-09 16:36:36 +0900

    methodの基礎について

---
+++ bronze/6.メソッド/Sample2.java
@@ -0,0 +1,9 @@
+class Sample2 {
+    public static void sample() {
+        System.out.println("B");
+    }
+    public static void main(String[] args) {
+        System.out.println("A");
+        sample();
+    }
+ }
