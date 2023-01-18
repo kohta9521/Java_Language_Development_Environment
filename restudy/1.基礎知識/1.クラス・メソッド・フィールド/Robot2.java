238fcfc180e64a0ed6da299d72f370c2055fe9de
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-18 12:47:45 +0900

    robot \2 files add

---
+++ restudy/1.基礎知識/1.クラス・メソッド・フィールド/Robot2.java
@@ -0,0 +1,17 @@
+class Robot2 {
+    int currentPosition = 0;
+    public static void main(String[] args) {
+        Robot robot = new Robot();
+        robot.report();
+        robot.moveForward();
+        robot.report();
+    }
+
+    void report() {
+        System.out.println("Current Position = " + currentPosition);
+    }
+
+    void moveForward() {
+        currentPosition = currentPosition + 1;
+    }
+}
