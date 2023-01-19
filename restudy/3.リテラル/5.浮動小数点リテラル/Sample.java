1d264d2b883f4e017689b2ef746c89810eeeba86
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-19 14:14:46 +0900

    浮動小数点のある会方

---
+++ restudy/3.リテラル/5.浮動小数点リテラル/Sample.java
@@ -0,0 +1,10 @@
+class Sample {
+    public static void main(String[] args) {
+        System.out.println(18.225);
+        System.out.println(5.23e3);
+        System.out.println(1.816e10);
+        System.out.println(7.5e-2);
+        System.out.println(2.712e-6);
+        System.out.println(3.52 + 17.4);
+    }
+}
