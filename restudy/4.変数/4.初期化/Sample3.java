80b023a229e3bf8c8b31046d4f0733b6c303a699
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-21 15:28:10 +0900

    add sample3

---
+++ restudy/4.変数/4.初期化/Sample3.java
@@ -0,0 +1,12 @@
+class Sample3 {
+    public static void main(String[] args) {
+        int result;
+
+        result = 84;
+        System.out.println(result);
+
+        result = 76;
+        System.out.println(result);
+
+    }
+}
