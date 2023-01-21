ec98ce6ef2474ddcdc807532569191fe9f762781
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-21 17:04:32 +0900

    add sample1

---
+++ restudy/4.変数/6.変数型推論/Sample.java
@@ -0,0 +1,10 @@
+class Sample {
+    public static void main(String[] args) {
+        var old = 24;
+        var name = "Tamada";
+
+        System.out.println(old);
+        System.out.println(name);
+
+    }
+}
