6e4f54a6d58ee488cf0441f07468fd88c040f665
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-22 15:55:11 +0900

    優先

---
+++ restudy/5.演算子/3.演算子の優先順位/Sample2.java
@@ -0,0 +1,7 @@
+class Sample2 {
+    public static void main(String[] args) {
+        int n1 = 10 - 5 - 3;
+
+        System.out.println(n1);
+    }
+}
---
+++ restudy/5.演算子/3.演算子の優先順位/Sample3.java
@@ -0,0 +1,11 @@
+class Sample3 {
+    public static void main(String[] args) {
+        int n1 = 10 + 8 / 2 * 4;
+        int n2 = (10 + 8  / 2) * 4;
+        int n3 = ((10 + 8) / 2) * 4;
+
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+    }
+}
