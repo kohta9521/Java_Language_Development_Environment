3c4a6805867152745dd3b75710a0cc5907656e03
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-22 17:12:09 +0900

    add sample

---
+++ restudy/5.演算子/4.インクリメントデクリメント/Sample.java
@@ -0,0 +1,18 @@
+class Sample {
+    public static void main(String[] args) {
+        int num = 5;
+
+        num ++;
+        System.out.println(num);
+
+        num --;
+        System.out.println(num);
+
+        ++ num;
+        System.out.println(num);
+
+        --num;
+        System.out.println(num);
+
+    }
+}
