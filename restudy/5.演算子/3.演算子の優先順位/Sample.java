8acf707172b5e514c4fa248dc10a860edc733bec
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-22 15:51:12 +0900

    add sample

---
+++ restudy/5.演算子/3.演算子の優先順位/Sample.java
@@ -0,0 +1,9 @@
+class Sample {
+    public static void main(String[] args) {
+        int n1 = 10 * 5 + 4;
+        int n2 = 4 + 10 * 5;
+
+        System.out.println(n1);
+        System.out.println(n2);
+    }
+}
