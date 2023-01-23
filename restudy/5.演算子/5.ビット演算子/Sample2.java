9223ac365fb268d3b3c1c3abb017d333eb0dd5d6
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-23 14:48:58 +0900

    add bit 演算　理解薄い

---
+++ restudy/5.演算子/5.ビット演算子/Sample2.java
@@ -0,0 +1,17 @@
+class Sample2 {
+    public static void main(String[] args) {
+        int i1 = 21 << 2;
+        int i2 = 21 >> 2;
+        int i3 = 21 >>> 2;
+
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+
+        int i4 = -92 >> 2;
+        int i5 = -92 >>> 2;
+
+        System.out.println(i4);
+        System.out.println(i5);
+    }
+}
