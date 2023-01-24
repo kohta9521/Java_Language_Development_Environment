e0abce75730db36359fc7766ceb77d3b30ced6b1
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-24 15:16:42 +0900

    add sample

--- restudy/7.条件分岐/1.if/Sample.java
+++ restudy/7.条件分岐/1.if/Sample.java
@@ -1,5 +1,15 @@
 class Sample {
     public static void main(String[] args) {
-        
+        System.out.println("1から10までの数値で偶数を出力します");
+
+        for (int i = 0; i <= 10; i ++) {
+            System.out.print(i + "は");
+
+            if (i % 2 == 0) {
+                System.out.println("偶数です");
+            } else {
+                System.out.println("奇数です");
+            }
+        }
     }
 }
