de306eeeda3aeddef813129be93c5fb8fc5ac663
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-26 00:43:18 +0900

    add sample1 配列の取得

---
+++ restudy/8.配列/13.配列の文字列取得/Sample.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Sample {
+    public static void main(String[] args) {
+        double[] src = {2/75, 0.52, 14.1};
+
+        System.out.println(src);
+        System.out.println(Arrays.toString(src));
+    }
+}
