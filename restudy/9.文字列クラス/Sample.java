634457e091b1234fd788f330bba91aa7ba480c6f
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-26 00:47:21 +0900

    add char[] add

---
+++ restudy/9.文字列クラス/Sample.java
@@ -0,0 +1,14 @@
+class Sample {
+    public static void main(String[] args) {
+        String msg1 = "Hello";
+
+        char[] c = {'B', 'y', 'e'};
+        String msg2 = new String(c);
+
+        String msg3 = new String(msg1);
+
+        System.out.println(msg1);
+        System.out.println(msg2);
+        System.out.println(msg3);
+    }
+}
