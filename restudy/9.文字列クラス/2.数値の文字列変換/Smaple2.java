61a7a0bab644c3c7408210375abbb2918c2db25f
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-26 16:47:17 +0900

    add からもじとの連結

---
+++ restudy/9.文字列クラス/2.数値の文字列変換/Smaple2.java
@@ -0,0 +1,23 @@
+class Smaple2 {
+    public static void main(String[] args) {
+        int i = 10;
+        short s = 21;
+        long l = 12L;
+        double d = 7.28;
+        boolean b = true;
+
+        String si = String.valueOf(i);
+        String ss = String.valueOf(s);
+        String sl = String.valueOf(l);
+        String sd = String.valueOf(d);
+        String sb = String.valueOf(b);
+
+        System.out.println(si);
+        System.out.println(ss);
+        System.out.println(sl);
+        System.out.println(sl);
+        System.out.println(sd);
+        System.out.println(sb);
+
+    }
+}
