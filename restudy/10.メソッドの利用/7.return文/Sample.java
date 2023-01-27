944785eff0e1db55d8a693a3bc8b5957e01e1741
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-27 20:27:27 +0900

    add2023/01/27 frist commit -

--- restudy/10.メソッドの利用/7.return文/Sample.java
+++ restudy/10.メソッドの利用/7.return文/Sample.java
@@ -1,5 +1,18 @@
 class Sample {
-    public static void main(String[]args) {
+    public static void main(String[] args) {
+        int data[];
 
+        test(15, 4);
+        test(7, 9);
+    }
+
+    private static void test(int n1, int n2) {
+        if (n2 == 0) {
+            System.out.println("0で割ることはできません");
+            return ;
+        }
+
+        System.out.println(n1 + " / " + n2 + " = " + (n1 / n2));
+        return;
     }
 }
