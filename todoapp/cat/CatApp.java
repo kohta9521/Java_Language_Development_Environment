76ce44a8bad4871853f09e097f38bee35dd818a0
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-28 18:50:57 +0900

    complete

--- todoapp/cat/CatApp.java
+++ todoapp/cat/CatApp.java
@@ -38,6 +38,18 @@ public class CatApp{
                 System.out.print("どの猫と遊びますか？");
                 int no = sc.nextInt();
                 list.get(no).play();
+                sortCat(list);
+            }
+        }
+    }
+    static void sortCat(ArrayList<Cat> list ) {
+        for (int i = 0; i < list.size() -1; i ++) {
+            for (int j = i + 1; j <list.size(); j ++) {
+                if(list.get(i).intimacy < list.get(j).intimacy) {
+                    Cat temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j,temp);
+                }
             }
         }
     }
