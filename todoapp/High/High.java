044e4f362002dbc131ceb64024c3147139cc2784
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-29 15:35:22 +0900

    add if

--- todoapp/High/High.java
+++ todoapp/High/High.java
@@ -9,5 +9,11 @@ public class High {
         String ans = new Scanner(System.in).next();
         int card2 = new Random().nextInt(13) + 1;
         System.out.println(card2);
+
+        if (ans.equalsIgnoreCase("High") && card1 < card2 || ans.equalsIgnoreCase("Low") && card1 > card2){
+            System.out.println("You Win!!!!");
+        } else {
+            System.out.println("You Lose...");
+        }
     }
 }
