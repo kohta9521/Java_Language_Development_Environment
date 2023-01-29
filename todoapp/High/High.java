8fe21bc5b3a40f569906f07c203881e2d0714269
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-29 15:28:45 +0900

    add hight basic file and basic class

---
+++ todoapp/High/High.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class High {
+    public static void main(String[] args) {
+        int card1 = new Random().nextInt(13) + 1;
+        System.out.println("1枚目のカードは" + card1 +  "です!!");
+    }
+}
