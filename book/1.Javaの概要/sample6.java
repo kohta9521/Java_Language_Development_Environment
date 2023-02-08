adedbe27bcc7c4a97fb210fff626554b1f98a878
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-08 17:38:19 +0900

    add samplr7

---
+++ book/1.Javaの概要/sample6.java
@@ -0,0 +1,9 @@
+class sample6 {
+  public static void main(String[] args) {
+    /*
+     * コメント
+     * サンプル
+     * ０台
+     */
+  }
+}
---
+++ book/1.Javaの概要/sample7.java
@@ -0,0 +1,7 @@
+class sample7 {
+  public static void main(String[] args) {
+    if (args.length != 0) {
+      System.out.println("hello world");
+    }
+  }
+}
